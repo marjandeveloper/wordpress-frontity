bfabc4d87f454acb71a20310aec295d348723fa0
anjaaleksa <marjan1505@gmail.com>
2021-04-26 16:03:56 +0200

    Added my project

---
+++ frontity.settings.js
@@ -0,0 +1,34 @@
+const settings = {
+  "name": "hello-frontity",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      name: "my-first-theme"
+   },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "https://test.frontity.org",
+          "postTypes": [
+            {
+              type: "destinations",
+              endpoint: "destinations",
+              archive: "/destinations"
+            }
+          ]
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
